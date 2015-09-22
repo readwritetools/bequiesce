d9f298a0c817f046a43ba03336647994b8ae349e
Joe Honton <joe@joe-honton.com>
2015-09-21 20:36:08 -0700

    05-test-harness.js and package.json first good exim effort

--- examples/05-spherical-coordinates/test/case/05-test-harness.js
+++ examples/05-spherical-coordinates/test/case/05-test-harness.js
@@ -1,15 +1,15 @@
 //=============================================================================
 //
-// File:         bequiesce/examples/05-spherical-coordinates/test-suite.js
+// File:         bequiesce/examples/05-spherical-coordinates/test/case/05-test-harness.js
 // Language:     ECMAScript 2015
 // Copyright:    Joe Honton © 2015
 // License:      CC-BY-NC-ND 4.0
-// Initial date: Sep 12, 2015
+// Initial date: Sep 21, 2015
 // Contents:     Bequiesce test runner for sphericoords.js 
 //
 //=============================================================================
 
-import Bequiesce from '../../lib/bequiesce.class';
+import Bequiesce from '../../../../src/bequiesce.class';
 
 Bequiesce.getInstance()
 	.testPackage("./test/case/sphericoords.1.test.js")
